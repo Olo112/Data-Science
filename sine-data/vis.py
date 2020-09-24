06bc8fa29c804c2760ad15ab779691b10455a556
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2020-09-24 22:58:25 +0200

    Commit: Update the visual data

--- sine-data/vis.py
+++ sine-data/vis.py
@@ -2,4 +2,9 @@ from matplotlib import pyplot as plt
 import data
 
 sine = data.sin_val()
-angle = data.angke()
+angle = data.angke(sine)
+
+plt.plot(angle, sine, color='blue', marker='o')
+
+plt.grid(True)
+plt.show()
